86b7dd79e4756d564d488283e39e718bb565956e
daliborturbeki <dturbeki@vub.hr>
2022-03-12 18:17:55 +0100

    home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,11 +11,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchBarComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,

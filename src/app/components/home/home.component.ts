493cce831b0a487ed7be2f80492b130535203876
daliborturbeki <dturbeki@vub.hr>
2022-03-13 08:29:58 +0100

    home display users

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { APIResponse } from 'src/app/models/apiResonse';
+import { User } from 'src/app/models/user';
+import { HttpService } from 'src/app/services/http.service';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   public sort: string = "";
+  public users: Array<User> = [];
 
-  constructor() { }
+  constructor(private httpService: HttpService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params: Params)=> {
+      if(params['user-search']) {
+        this.searchUsers('name', params['user-search']);
+      } else {
+        this.searchUsers('name');
+      }
+    });
+  }
+  searchUsers(sort: string, search?: string): void{
+    this.httpService.getUsersList(sort, search).subscribe((usersList: User[]) => {
+      this.users = usersList;
+      console.log(usersList);
+    });
   }
 
 }
